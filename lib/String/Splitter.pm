3b027d7555d1e8c3348a91abdb87ead90ff605e9
Paweł (bbkr) Pabian <bbkr@cpan.org>
2008-03-19 13:14:30 +0000

    Import of BBKR/String-Splitter-0.3 from CPAN.
    gitpan-cpan-distribution: String-Splitter
    gitpan-cpan-version:      0.3
    gitpan-cpan-path:         BBKR/String-Splitter-0.3.tar.gz
    gitpan-cpan-author:       BBKR
    gitpan-cpan-maturity:     released

--- lib/String/Splitter.pm
+++ lib/String/Splitter.pm
@@ -6,17 +6,19 @@ use utf8;
 use warnings;
 use strict;
 
+=encoding UTF-8
+
 =head1 NAME
 
 String::Splitter - Find all possible string splits and unique substrings.
 
 =head1 VERSION
 
-Version 0.2
+Version 0.3
 
 =cut
 
-our $VERSION = '0.2';
+our $VERSION = '0.3';
 
 =head1 SYNOPSIS
 
